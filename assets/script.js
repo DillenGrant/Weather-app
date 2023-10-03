eb6d60891d06fa1cf694f79c3e0bf0d64a757258
DillenGrant <dillen.grant72@gmail.com>
2023-10-02 18:00:53 -0600

    new direction

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-const apiKey = '2c48023ff32014febde54e6be23396d0';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial'
+const apiKey = "2c48023ff32014febde54e6be23396d0";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=germany&units=imperial";
 
 const cityForm = document.getElementById('city-form');
 const cityInput = document.getElementById('city-input');
@@ -7,9 +7,9 @@ const searchHistory = document.getElementById('search-history');
 const currentWeather = document.getElementById('current-weather');
 const forecast = document.getElementById('forecast');
 
-async function getWeatherData(cityName) {
-    try {
-        const response = await fetch(apiUrl + '?q=${cityName}&appid=${apiKey}&units=imperial');
+async function getWeatherData() {
+    try{
+        const response = await fetch(apiUrl + `&appid=${apiKey}` );
         const data = await response.json();
         return data;
     } catch (error) {
@@ -21,21 +21,22 @@ async function getWeatherData(cityName) {
 cityForm.addEventListener('submit', async function (e) {
     e.preventDefault();
     const cityName = cityInput.value.trim();
+
     if (cityName) {
+        // Fetch current weather data
         const currentWeatherData = await getWeatherData(cityName);
+
         if (currentWeatherData) {
-    // Use an API (e.g., OpenWeatherMap) to fetch weather data for the city
-    // Display current and future weather conditions
-          // Add the city to the search history
+          document.querySelector()
         }
     }
-
-    
 });
 
+// Event listener for clicking on a city in the search history
 searchHistory.addEventListener('click', function (e) {
     if (e.target.tagName === 'LI') {
         const cityName = e.target.textContent;
+        
+        // Fetch and display weather data for the selected city (similar to the form submission)
     }
-    fetch
 });
